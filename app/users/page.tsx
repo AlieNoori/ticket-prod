df02639c68b69c45f4afdb02e6e835c9c31353af
alieNoori <alli.nouri.83@gmail.com>
2024-08-11 23:29:22 +0330

    added users table data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,13 @@
 import UserForm from '@/components/UserForm';
+import DataTableSimple from './DataTableSimple';
+import prisma from '@/prisma/db';
 
-function Users() {
+async function Users() {
+  const users = await prisma.user.findMany();
   return (
     <div>
       <UserForm />
+      <DataTableSimple users={users} />
     </div>
   );
 }
